e1003d01fa9e52b4e8024234e30aabea7979f621
Karlo Dini Kožić <karlodini.kozic@outlook.com>
2024-02-19 14:30:48 +0100

    basic data visualization

--- frontend/src/pages/Main/Main.tsx
+++ frontend/src/pages/Main/Main.tsx
@@ -1,22 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useNetworkLayer from '../../hooks/networkLayer/useNetworkLayer';
+import { CityData } from '../../hooks/networkLayer/types/types';
+import './style.scss'
 
 function Main() {
+
+  const [cities, setCities] = useState<CityData[]>([]);
 
   const  {client} = useNetworkLayer()
 
   const handleOnClick =async () => {
     const data =   await client.getCityData()
-    console.log(data.cities);
+    setCities(data.cities)
   }
 
 
   return (
-    <div>Main
-      <button onClick={handleOnClick}>presmee</button>
+    <div className='Main'>
+      Main
+      <button onClick={handleOnClick}>Press</button>
+      <h1>City Explorer</h1>
+      <div className="city-container">
+        {cities.map((city, index) => (
+          <div className="city-card" key={index}>
+            <h2>{city.name}</h2>
+            <p><strong>Country:</strong> {city.country}</p>
+            <p><strong>Continent:</strong> {city.continent}</p>
+            <p><strong>Population:</strong> city.population)</p>
+            <p><strong>Founded:</strong> {city.founded}</p>
+            <h3>Landmarks:</h3>
+            <ul>
+              {city.landmarks.map((landmark, index) => (
+                <li key={index}>{landmark}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
     </div>
 
-  );
+)
+  ;
 }
 
 export default Main;
