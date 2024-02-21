09be7871a03b4456677a2cdada599b87c899eb7b
Karlo Dini Kožić <karlodini.kozic@outlook.com>
2024-02-21 14:54:46 +0100

    Added backend tests coverage

--- backend/src/data/controllers/data.controller.spec.ts
+++ backend/src/data/controllers/data.controller.spec.ts
@@ -1,18 +1,60 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DataController } from './data.controller';
+import { DataService } from '../services/data.service';
+import { HttpStatus } from '@nestjs/common';
 
 describe('DataController', () => {
   let controller: DataController;
+  let dataService: DataService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [DataController],
+      providers: [DataService],
     }).compile();
 
     controller = module.get<DataController>(DataController);
+    dataService = module.get<DataService>(DataService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  describe('GetData', () => {
+    it('should return data successfully', async () => {
+      const mockData = [{ name: 'City 1' }, { name: 'City 2' }];
+      jest.spyOn(dataService, 'getData').mockReturnValueOnce(mockData);
+
+      const responseMock = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      } as any;
+
+      await controller.GetData(responseMock);
+
+      expect(responseMock.status).toHaveBeenCalledWith(HttpStatus.OK);
+      expect(responseMock.json).toHaveBeenCalledWith({ data: mockData });
+    });
+
+    it('should handle errors and return 500 status code', async () => {
+      const errorMessage = 'Error occurred';
+      jest.spyOn(dataService, 'getData').mockImplementationOnce(() => {
+        throw new Error(errorMessage);
+      });
+
+      const responseMock = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      } as any;
+
+      await controller.GetData(responseMock);
+
+      expect(responseMock.status).toHaveBeenCalledWith(
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+      expect(responseMock.json).toHaveBeenCalledWith({ error: errorMessage });
+    });
+  });
+
 });
--- backend/src/data/services/data.service.spec.ts
+++ backend/src/data/services/data.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DataService } from './data.service';
+import * as fs from 'fs';
 
 describe('DataService', () => {
   let service: DataService;
@@ -14,5 +15,36 @@ describe('DataService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getData', () => {
+    it('should return JSON data from file', () => {
+      // Mocking environment variable
+      process.env.PATH_TO_CITY_JSON = 'path/to/mock/cities.json';
+
+      // Mocking fs.readFileSync to return sample data
+      const mockData = [{ name: 'City 1' }, { name: 'City 2' }];
+      jest
+        .spyOn(fs, 'readFileSync')
+        .mockReturnValueOnce(JSON.stringify(mockData));
+
+      const result = service.getData();
+
+      expect(result).toEqual(mockData);
+    });
+
+    it('should throw an error if file reading fails', () => {
+      // Mocking environment variable
+      process.env.PATH_TO_CITY_JSON = 'nonexistent/file.json';
+
+      // Mocking fs.readFileSync to throw an error
+      jest.spyOn(fs, 'readFileSync').mockImplementation(() => {
+        throw new Error('Error while read cites.json file');
+      });
+
+      expect(() => service.getData()).toThrow(
+        'Error while read cites.json file',
+      );
+    });
   });
 });
